9a0630742100485c1e7b335665ce0d21c76e778d
Dariel <djdaariel@gmail.com>
2024-06-05 19:13:09 -0600

    Se arregla error de netlifgy

--- netlify/edge-functions/server/server.js
+++ netlify/edge-functions/server/server.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from 'apollo-server';
 
-import typeDefs from '../../src/graphql/typeDefs';
-import resolvers from '../../src/graphql/resolvers';
+import typeDefs from '../../../src/graphql/typeDefs';
+import resolvers from '../../../src/graphql/resolvers';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
