d4595932a02169829f773813e094c65f0780117d
Dariel <djdaariel@gmail.com>
2024-06-05 19:51:38 -0600

    Ser arreglan errores

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,6 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
 const typeDefs = require('../../src/graphql/typeDefs');
-const resolvers = require('../../graphql/resolvers');
+const resolvers = require('../../src/graphql/resolvers');
+
 
 const server = new ApolloServer({
   typeDefs,
